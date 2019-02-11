8a91ac42e06750d2c77d9b34fab633512a35bbb5
Observed Observer <MasterHaoChen@icloud.com>
2019-02-12 00:23:58 +0800

    better test build port setting

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const Config = {
   // host: '//lvmaozi.info',
   host: '//localhost',
-  // port: 8000,
-  port: 8080,
+  port: 8000,
+  // port: 8080,
   getURL: (service) => {
     return `${Config.host}:${Config.port}/${service}`
   }

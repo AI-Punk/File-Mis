72cf0e4504a974b0159e01e8173a5e4fbd90b071
ObservedObserver <270001151@qq.com>
2019-03-18 12:34:29 +0800

    Update pdf.js

--- src/pages/manager/home/pdf.js
+++ src/pages/manager/home/pdf.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 class PDFReader extends Component {
   render () {
     return (
-      <iframe style={{width: '100%', height: '540px'}} src={this.props.src} title="file"></iframe>
+      <iframe frameborder="1" style={{width: '100%', height: '540px'}} src={this.props.src + '#toolbar=0&navpanes=0'} title="file"></iframe>
     )
   }
 }

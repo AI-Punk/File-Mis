54963edeb22af092b23c1d3b1779bc148299d0ca
Observed Observer <MasterHaoChen@icloud.com>
2019-09-07 00:23:27 +0800

    feat IU004: add hint for access limit

--- src/pages/manager/home/video.js
+++ src/pages/manager/home/video.js
@@ -1,9 +1,11 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import { message } from 'antd';
 
 class VideoReader extends Component {
   timeLimit = (ev) => {
     if (ev.target.currentTime > this.refs.video.duration * this.props.limit) {
-      this.refs.video.currentTime = 0
+      this.refs.video.currentTime = 0;
+      message.error(`You only have ${Math.round(this.props.limit * 100)} % access of this resource.`);
     }
   }
   render () {

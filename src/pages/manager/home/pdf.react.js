2189e8b1a463fed79c340bfc8cbdedbe11a7702c
Observed Observer <MasterHaoChen@icloud.com>
2019-09-15 12:37:22 +0800

    test: use cdn for pdf worker

--- src/pages/manager/home/pdf.react.js
+++ src/pages/manager/home/pdf.react.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Document, Page } from 'react-pdf';
-
+import { Document, Page, pdfjs } from 'react-pdf';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 class MyApp extends Component {
   state = {
     numPages: null,
